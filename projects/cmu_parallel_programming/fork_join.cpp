32e5b076c1d61d0394512d1136f4449dea7681f8
MenciusChin <menciuschin@gmail.com>
2023-10-10 14:08:09 -0400

    add forkjoin example

---
+++ projects/cmu_parallel_programming/fork_join.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+long fib_seq (long n) {
+  long result;
+  if (n < 2) {
+    result = n;
+  } else {
+    long a, b;
+    a = fib_seq(n-1);
+    b = fib_seq(n-2);
+    result = a + b;
+  }
+  return result;
+}
+
+
+int main()
+{
+    long n = 10;
+
+    cout << "The Fib num for n = " << n << " would be: " << fib_seq(n);
+}
