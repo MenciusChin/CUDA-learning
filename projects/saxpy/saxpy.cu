c2d35e81be8822c6444980895a326db64926fc99
MenciusChin <menciuschin@gmail.com>
2023-10-12 22:17:05 -0400

    unicoding problem of saxpy.cu

--- projects/saxpy/saxpy.cu
+++ projects/saxpy/saxpy.cu
@@ -11,36 +11,45 @@ __global__ void saxpy(int n, float a, float *x, float *y)
   if (i < n) y[i] = a*x[i] + y[i];
 }
 
+
 int main(void)
 {
-  int N = 1<<20;
-  float *x, *y, *d_x, *d_y;
-  x = (float*)malloc(N*sizeof(float));
-  y = (float*)malloc(N*sizeof(float));
+    // Initialize variables
+    int N = 1<<20;
+    float *x, *y, *d_x, *d_y;
 
-  cudaMalloc(&d_x, N*sizeof(float)); 
-  cudaMalloc(&d_y, N*sizeof(float));
+    // Allocate host memories
+    x = (float*)malloc(N*sizeof(float));
+    y = (float*)malloc(N*sizeof(float));
 
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
+    // Allocate device memories
+    cudaMalloc(&d_x, N*sizeof(float)); 
+    cudaMalloc(&d_y, N*sizeof(float));
 
-  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+    // Initialize host arrays
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
 
-  // Perform SAXPY on 1M elements
-  saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
+    // Initialize device arrays
+    cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
 
-  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+    // Perform SAXPY on 1M elements
+    saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
 
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = max(maxError, abs(y[i]-4.0f));
-  printf("Max error: %f\n", maxError);
+    // Retrive result back to host
+    cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
 
-  cudaFree(d_x);
-  cudaFree(d_y);
-  free(x);
-  free(y);
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = max(maxError, abs(y[i]-4.0f));
+    printf("Max error: %f\n", maxError);
+
+    // Cleaning up memories
+    cudaFree(d_x);
+    cudaFree(d_y);
+    free(x);
+    free(y);
 }
